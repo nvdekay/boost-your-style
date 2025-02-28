6aaf696a4580969d63c1895804f3271ecd3e77d1
nvdekay <khanhnvd.work@gmail.com>
2025-02-28 20:52:26 +0700

    fixed in LoginFilter

--- src/java/filter/LoginFilter.java
+++ src/java/filter/LoginFilter.java
@@ -15,7 +15,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter(filterName = "LoginFilter", urlPatterns = { "/*" })
+@WebFilter(filterName = "LoginFilter", urlPatterns = {"/*"})
 public class LoginFilter implements Filter {
 
     private static final boolean debug = true;
@@ -30,27 +30,6 @@ public class LoginFilter implements Filter {
         if (debug) {
             log("LoginFilter:DoBeforeProcessing");
         }
-
-        // Write code here to process the request and/or response before
-        // the rest of the filter chain is invoked.
-        // For example, a logging filter might log items on the request object,
-        // such as the parameters.
-        /*
-         * for (Enumeration en = request.getParameterNames(); en.hasMoreElements(); ) {
-         * String name = (String)en.nextElement();
-         * String values[] = request.getParameterValues(name);
-         * int n = values.length;
-         * StringBuffer buf = new StringBuffer();
-         * buf.append(name);
-         * buf.append("=");
-         * for(int i=0; i < n; i++) {
-         * buf.append(values[i]);
-         * if (i < n-1)
-         * buf.append(",");
-         * }
-         * log(buf.toString());
-         * }
-         */
     }
 
     private void doAfterProcessing(ServletRequest request, ServletResponse response)
@@ -59,23 +38,6 @@ public class LoginFilter implements Filter {
             log("LoginFilter:DoAfterProcessing");
         }
 
-        // Write code here to process the request and/or response after
-        // the rest of the filter chain is invoked.
-        // For example, a logging filter might log the attributes on the
-        // request object after the request has been processed.
-        /*
-         * for (Enumeration en = request.getAttributeNames(); en.hasMoreElements(); ) {
-         * String name = (String)en.nextElement();
-         * Object value = request.getAttribute(name);
-         * log("attribute: " + name + "=" + value.toString());
-         * 
-         * }
-         */
-        // For example, a filter might append something to the response.
-        /*
-         * PrintWriter respOut = new PrintWriter(response.getWriter());
-         * respOut.println("<P><B>This has been appended by an intrusive filter.</B>");
-         */
     }
 
     public void doFilter(ServletRequest request, ServletResponse response,
@@ -100,17 +62,12 @@ public class LoginFilter implements Filter {
         try {
             chain.doFilter(request, response);
         } catch (Throwable t) {
-            // If an exception is thrown somewhere down the filter chain,
-            // we still want to execute our after processing, and then
-            // rethrow the problem after that.
             problem = t;
             t.printStackTrace();
         }
 
         doAfterProcessing(request, response);
 
-        // If there was a problem, we want to rethrow it if it is
-        // a known type, otherwise log it.
         if (problem != null) {
             if (problem instanceof ServletException) {
                 throw (ServletException) problem;
@@ -161,16 +118,16 @@ public class LoginFilter implements Filter {
                 response.setContentType("text/html");
                 PrintStream ps = new PrintStream(response.getOutputStream());
                 PrintWriter pw = new PrintWriter(ps);
-                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); 
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n");
                 // PENDING! Localize this for next official release
                 pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
                 pw.print(stackTrace);
-                pw.print("</pre></body>\n</html>"); 
+                pw.print("</pre></body>\n</html>");
                 pw.close();
                 ps.close();
                 response.getOutputStream().close();
             } catch (Exception ex) {
-                
+
             }
         } else {
             try {
