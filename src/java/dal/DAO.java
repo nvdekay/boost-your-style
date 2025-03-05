59d9df0336bd76b7522f39d702649034ec9c4ef4
nvdekay <khanhnvd.work@gmail.com>
2025-03-05 16:41:46 +0700

    updated DAO_getCategoryById and getNewProducts

--- src/java/dal/DAO.java
+++ src/java/dal/DAO.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import model.Product;
 
 public class DAO extends DBContext {
 
@@ -43,6 +44,49 @@ public class DAO extends DBContext {
                         rs.getString("name"),
                         rs.getString("describe"));
                 list.add(c);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+
+    public Category getCategoryById(int id) {
+        String sql = "Select * from Categories where id = ?";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Category c = new Category(rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("describe"));
+                return c;
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public List<Product> getNewProducts() {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT TOP 3 * FROM Products ORDER BY releaseDate DESC";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Product p = new Product();
+                p.setId(rs.getString("id"));
+                p.setName(rs.getString("name"));
+                p.setQuantity(rs.getInt("quantity"));
+                p.setPrice(rs.getDouble("price"));
+                p.setReleaseDate(rs.getString("releaseDate"));
+                p.setDescribe(rs.getString("describe"));
+                p.setImage(rs.getString("image"));
+                Category c = getCategoryById(rs.getInt("cid"));
+                p.setCategory(c);
+                list.add(p);
             }
         } catch (SQLException e) {
             System.out.println(e);
