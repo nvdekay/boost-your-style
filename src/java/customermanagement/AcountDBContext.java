71c3d2048627b05bafbe1f0556512dbf6df11854
nvdekay <khanhnvd.work@gmail.com>
2025-03-17 00:15:46 +0700

    add package customermanagement

---
+++ src/java/customermanagement/AcountDBContext.java
@@ -0,0 +1,167 @@
+package customermanagement;
+
+import dal.DBContext;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Account;
+import model.Product;
+import model.Role;
+
+public class AcountDBContext extends DBContext {
+
+    public Account checkAccountExist(String username) {
+        try {
+            String sql = "SELECT id , username , password , active FROM Account where [username] = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, username);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Account a = new Account();
+                a.setId(rs.getInt(1));
+                a.setUsername(rs.getString(2));
+                a.setPassword(rs.getString(3));
+                a.setActive(rs.getBoolean(4));
+                return a;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AcountDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public List<Account> getAllAccount() {
+        List<Account> list = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM Account where [roleId] != 1";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Account account = mapAccount(rs);
+
+                list.add(account);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(AcountDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    public Account mapAccount(ResultSet rs) throws SQLException {
+        Account account = new Account();
+        account.setId(rs.getInt(1));
+        account.setUsername(rs.getString(2));
+        account.setEmail(rs.getString(3));
+        account.setAddress(rs.getString(4));
+        account.setPhoneNumber(rs.getString(5));
+        account.setPassword(rs.getString(6));
+        account.setRoleId(rs.getInt(7));
+        account.setActive(rs.getBoolean(9));
+        return account;
+    }
+
+    public List<Account> getAllAccountByPage(int page, int PAGE_SIZE) {
+        List<Account> list = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM Account where roleId != 1"
+                    + " order by id\n"
+                    + "offset (?-1)*? row fetch next ? rows only";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, page);
+            stm.setInt(2, PAGE_SIZE);
+            stm.setInt(3, PAGE_SIZE);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Account account = mapAccount(rs);
+
+                list.add(account);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(AcountDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+
+    public int getTotalAccount() {
+        List<Account> list = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM Account where roleId != 1";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Account account = mapAccount(rs);
+
+                list.add(account);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(AcountDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list.size();
+    }
+
+    public void UpDatePassWord(String pass, int id) {
+        try {
+            String sql = "UPDATE [Account]\n"
+                    + "   SET [password] = ?\n"
+                    + " WHERE [id] = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, pass);
+            stm.setInt(2, id);
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(AcountDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public int insertAccount(Account account) {
+        try {
+            String sql = "INSERT INTO [Account]\n"
+                    + "           ([username]\n"
+                    + "           ,[password]\n"
+                    + "           ,[email]\n"
+                    + "           ,[address]\n"
+                    + "           ,[active]\n"
+                    + "           ,[roleId]\n"
+                    + "           ,[phoneNumber])\n"
+                    + "     VALUES\n"
+                    + "           (?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,1\n"
+                    + "           ,2\n" // role customer
+                    + "           ,?)";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, account.getUsername());
+            stm.setString(2, account.getPassword());
+            stm.setString(3, account.getEmail());
+            stm.setString(4, account.getAddress());
+            stm.setString(5, account.getPhoneNumber());
+            return stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(AcountDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+
+    public void updateAccount(Account account) {
+
+        try {
+            String sql = "UPDATE [Account]\n"
+                    + "   SET [active] = ?\n"
+                    + " WHERE id = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setBoolean(1, account.isActive());
+            stm.setInt(2, account.getId());
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(AcountDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
---
+++ src/java/customermanagement/ManagerAccountController.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customermanagement;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Account;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "ManagerAccountController", urlPatterns = {"/managerAccount"})
+public class ManagerAccountController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (request.getSession().getAttribute("account") != null && request.getSession().getAttribute("role_admin") != null) {
+            AcountDBContext acountDBContext = new AcountDBContext();
+            // page
+            final int PAGE_SIZE = 3;
+            int page = 1;
+            String pageStr = request.getParameter("page");
+            if (pageStr != null) {
+                page = Integer.parseInt(pageStr);
+            }
+            if (page < 1) {
+                page = 1;
+            }
+            // lấy số lượng account
+            int totalAccount = acountDBContext.getTotalAccount();
+            int totalPage = totalAccount / PAGE_SIZE;
+            if (totalAccount % PAGE_SIZE != 0) {
+                totalPage += 1;
+            }
+            if (page > totalPage) {
+                page = totalPage;
+            }
+            request.setAttribute("page", page);
+            request.setAttribute("totalPage", totalPage);
+            List<Account> accounts = acountDBContext.getAllAccountByPage(page, PAGE_SIZE);
+            request.setAttribute("accounts", accounts);
+            request.getRequestDispatcher("ManagerCustomer.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (request.getSession().getAttribute("account") != null && request.getSession().getAttribute("role_admin") != null) {
+            // Lấy dữ liệu từ form
+            String email = request.getParameter("email");
+            String phoneNumber = request.getParameter("phoneNumber");
+            String address = request.getParameter("address");
+
+            // Username = email, Password = mặc định hoặc có thể tạo random
+            String username = email;
+            String password = "1"; // Hoặc mã hóa password trước khi lưu
+
+            // Tạo đối tượng Account
+            Account account = new Account(username, email, address, phoneNumber, password);
+
+            // Gọi phương thức insert
+            AcountDBContext db = new AcountDBContext();
+            db.insertAccount(account);
+            response.sendRedirect("managerAccount?page=1");
+        } else {
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
